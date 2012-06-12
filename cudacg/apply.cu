4164d2e1a1382fe6552aee2f2b217ca7bc2cbb21
Sinan <sinan@ubuntu.ubuntu-domain>
2012-06-12 12:41:21 +0100

    gpu apply

--- cudacg/apply.cu
+++ cudacg/apply.cu
@@ -44,41 +44,27 @@ __global__ void gpu_apply_kernel(const N *n, const float*x, float *y){//(const N
  
 }
 
-int gpu_apply(const N n, const REAL *x, REAL *y){
+int gpu_apply(const N *n, const REAL *dev_x, REAL *dev_y){
 
-  //float *x,*y,*gpu_y;
-  //cudaMallocHost((void**)&x,len*sizeof(REAL));
-  //cudaMallocHost((void**)&y,len*sizeof(REAL));
-  //cudaMallocHost((void**)&gpu_y,len*sizeof(REAL));
   
-  int len=n.x*n.y*n.z;
-  N *dev_n,*l_n;
-  REAL *dev_x, *dev_y;
+  N *dev_n;
+   
   
-  cudaMallocHost((void**)&l_n,sizeof(N));
   cudaMalloc((void**)&dev_n,sizeof(N));
-  cudaMalloc((void**)&dev_x,len*sizeof(REAL));
-  cudaMalloc((void**)&dev_y,len*sizeof(REAL));
-
-  l_n->x=n.x;
-  l_n->y=n.y;
-  l_n->z=n.z;
 
 
-  cudaMemcpy(dev_n,l_n,sizeof(N),cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_x,x,len*sizeof(REAL),cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_n,n,sizeof(N),cudaMemcpyHostToDevice);
   dim3 dimBlock(BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE);
-  dim3 dimGrid(n.x/BLOCK_SIZE,n.y/BLOCK_SIZE,n.z/BLOCK_SIZE);
- 
+  dim3 dimGrid(n->x/BLOCK_SIZE,n->y/BLOCK_SIZE,n->z/BLOCK_SIZE);
+  //dim3 dimGrid(n->x,n->y,n->z);
 
   gpu_apply_kernel<<<dimGrid,dimBlock>>>(dev_n,dev_x,dev_y);
 
-  cudaMemcpy(y,dev_y,len*sizeof(REAL),cudaMemcpyDeviceToHost);
     
 
   cudaFree(dev_n);
-  cudaFree(dev_x);
-  cudaFree(dev_y);
+  //cudaFree(dev_x);
+  //cudaFree(dev_y);
  
   
 
